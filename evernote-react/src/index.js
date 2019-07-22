8d5beeaa5681550f40d5e91a49716cb86708ee04
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-07-22 16:43:39 -0600

    Add firebase SDK

--- evernote-react/src/index.js
+++ evernote-react/src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoI5uce9zsoFkSgKDDYgElmK4u1tidgLc",
+  authDomain: "evernote-clone-c09dc.firebaseapp.com",
+  databaseURL: "https://evernote-clone-c09dc.firebaseio.com",
+  projectId: "evernote-clone-c09dc",
+  storageBucket: "evernote-clone-c09dc.appspot.com",
+  messagingSenderId: "262234473127",
+  appId: "1:262234473127:web:e8489e28aac214de"
+};
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
