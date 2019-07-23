1bb54176626d24a4c9dd91e309c4af9c85c3235c
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-07-23 12:07:48 -0600

    Display note's title and body on sidebar items

--- evernote-react/src/sidebaritem/sidebaritem.js
+++ evernote-react/src/sidebaritem/sidebaritem.js
@@ -7,15 +7,30 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { removeHTMLTags } from '../helpers';
 
 class SidebarItemComponent extends React.Component {
-  constructor() {
-    super();
-  }
 
   render() {
+
+    const { _note, _index, classes, selectedNoteIndex } = this.props;
+
     return (
-      <div>Hello from Sidebar Item</div>
+      <div key={_index}>
+        <ListItem
+          className={classes.listItem}
+          selected={this.selectedNoteIndex === _index}
+          alignItems='flex-start'>
+          
+          <div 
+            className={classes.textSection}
+            onClick={() => this.selectNote(_note, _index)}>
+              <ListItemText
+                primary={_note.title}
+                secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}></ListItemText>
+          </div>
+        </ListItem>
+      </div>
     );
   }
+
 }
 
 export default withStyles(styles)(SidebarItemComponent);
