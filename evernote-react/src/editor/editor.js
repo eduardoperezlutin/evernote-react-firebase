2631cb4b2a6dc9e232bd67702353cbc4d64a9e25
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-07-23 17:06:36 -0600

    Update note title

--- evernote-react/src/editor/editor.js
+++ evernote-react/src/editor/editor.js
@@ -40,6 +40,14 @@ class EditorComponent extends React.Component {
 
     return (
       <div className={classes.editorContainer}>
+        <BorderColorIcon className={classes.editIcon}></BorderColorIcon>
+
+        <input
+          className={classes.titleInput}
+          placeholder='Note title'
+          value={this.state.title ? this.state.title : ''}
+          onChange={(e) => this.updateTitle(e.target.value)}></input>
+
         <ReactQuill 
           value={this.state.text}
           onChange={this.updateBody}>
@@ -53,6 +61,13 @@ class EditorComponent extends React.Component {
     await this.setState({ text: val });
     this.update();
   };
+
+  updateTitle = async (txt) => {
+    await this.setState({
+      title: txt
+    });
+    this.update();
+  }
 
   update = debounce(() => {
     this.props.noteUpdate(this.state.id, {
